3536641d492f5c4daa057db985a39ff033eef944
leonardz1z <50324810+leonardz1z@users.noreply.github.com>
2022-11-03 17:40:51 +0700

    update permission for download

--- app/Http/Controllers/Cms/PhotoController.php
+++ app/Http/Controllers/Cms/PhotoController.php
@@ -41,7 +41,8 @@ class PhotoController extends Controller
 
         if (!File::exists($path)) {
             File::makeDirectory($path, 0777, true);
-            // exec("chown -R root:apache " . $path);
+            exec("chmod -R 775 " . $path);
+            exec("chown -R www-data:www-data " . $path);
         }   
         $fileName = 'download-photos.zip';
 
