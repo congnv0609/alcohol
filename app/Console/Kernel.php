284e05b24c019d74349f409271accd8ea2de68a2
leonardz1z <50324810+leonardz1z@users.noreply.github.com>
2022-11-28 11:18:56 +0700

    change confition to push

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,15 +37,15 @@ class Kernel extends ConsoleKernel
 
         if (!empty($data)) {
             foreach ($data as $key => $value) {
-                if ($value["completed"]!= 8886 && $value["nth_popup"] == 0) {
+                if ($value["completed"] == 8886 && $value["nth_popup"] == 0) {
                     $time = date_format(new DateTime($value["popup_time"]), 'H:i');
                     $schedule->job(new SendNotification($value))->daily()->at($time);
                 }
-                if ($value["completed"] != 8886 && $value["nth_popup"] == 1) {
+                if ($value["completed"] == 8886 && $value["nth_popup"] == 1) {
                     $time1 = date_format(new DateTime($value["popup_time1"]), 'H:i');
                     $schedule->job(new SendNotification($value))->daily()->at($time1);
                 }
-                if ($value["completed"] != 8886 && $value["nth_popup"] == 2) {
+                if ($value["completed"] == 8886 && $value["nth_popup"] == 2) {
                     $time2 = date_format(new DateTime($value["popup_time2"]), 'H:i');
                     $schedule->job(new SendNotification($value))->daily()->at($time2);
                 }
